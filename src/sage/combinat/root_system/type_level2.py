34b93ec0f8863bd908b955c7a226ec1d391971f1
SamyMekkati <samy.mekkati.1@ens.etsmtl.ca>
2025-08-04 16:43:41 -0400

    classification complete

--- src/sage/combinat/root_system/type_level2.py
+++ src/sage/combinat/root_system/type_level2.py
@@ -26,9 +26,32 @@ class CoxeterType_Level2(CoxeterType):
     """
     def __init__(self, data):
         self._key = tuple(data)
+        self._category = data[0]
+        self._rank = data[1]
+        self._position = data[2]
 
         super().__init__()
 
+    def _repr_(self):
+        """
+        Return a string representation of ``self``.
+
+        This method returns a string that describes the Coxeter type,
+        including the reference to Chen-Labbé's articles mentioned above.
+
+        EXAMPLES::
+
+            sage: L2 = CoxeterType(["Cy", 5, (5, 2)])
+            sage: L2
+            Coxeter type of ['Cy', 5] at line 5 column 2
+        """
+        a, b = self._position
+
+        return (
+                f"Coxeter type of ['{self._category}', {self._rank}] "
+                f"with position (Row : {a}, Column : {b})"
+        )
+    
     def rank(self):
         return len(level2_matrices[self._key])
 
